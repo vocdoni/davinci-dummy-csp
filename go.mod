fc9835b7db5f9da72841008dbdc1d354b5c1a4f0
Lucas Menéndez <hi@lucasmenendez.me>
2025-09-15 12:20:42 +0200

    initial code

---
+++ go.mod
@@ -0,0 +1,38 @@
+module github.com/vocdoni/davinci-dummy-csp
+
+go 1.24.2
+
+replace github.com/consensys/gnark => github.com/vocdoni/gnark-no-assert v0.0.0-20250723103948-9e2777846d75
+
+require (
+	github.com/ethereum/go-ethereum v1.16.1
+	github.com/vocdoni/davinci-node v0.0.0-20250915100213-079ba796c455
+)
+
+require (
+	github.com/bits-and-blooms/bitset v1.22.0 // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/consensys/gnark v0.13.0 // indirect
+	github.com/consensys/gnark-crypto v0.18.0 // indirect
+	github.com/dchest/blake512 v1.0.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/google/pprof v0.0.0-20250630185457-6e76a2b096b5 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
+	github.com/iden3/go-iden3-crypto v0.0.17 // indirect
+	github.com/ingonyama-zk/icicle-gnark/v3 v3.2.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pressly/goose/v3 v3.21.1 // indirect
+	github.com/ronanh/intcomp v1.1.1 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
+	github.com/vocdoni/arbo v0.0.0-20250904220709-07ccfd3090b5 // indirect
+	github.com/vocdoni/gnark-crypto-primitives v0.0.2-0.20250721114051-04061c85ab97 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/exp v0.0.0-20250717185816-542afb5b7346 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/vocdoni/davinci-node/crypto/csp"
+	"github.com/vocdoni/davinci-node/types"
+	"github.com/vocdoni/davinci-node/util"
+)
+
+func main() {
+	// Load the seed from an environment variable
+	strSeed := os.Getenv("CSP_SEED")
+	if strSeed == "" {
+		log.Fatal("CSP_SEED environment variable is required")
+	}
+	strHTTPPort := os.Getenv("CSP_PORT")
+	if strHTTPPort == "" {
+		strHTTPPort = "8080" // Default port
+	}
+
+	// Create a new CSP with the correct origin and seed provided
+	dummyCSP, err := csp.New(types.CensusOriginCSPEdDSABLS12377, []byte(strSeed))
+	if err != nil {
+		log.Fatalf("Error creating CSP: %v", err)
+	}
+
+	// Calculate the census root
+	censusRoot := dummyCSP.CensusRoot()
+	log.Printf("CSP initialized, census root: %x", censusRoot.Root)
+
+	// Endpoint to return the census root
+	http.HandleFunc("/root", func(w http.ResponseWriter, r *http.Request) {
+		res, err := json.Marshal(censusRoot)
+		if err != nil {
+			http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	})
+
+	// Endpoint to get a census proof for a given process ID and address
+	http.HandleFunc("/proof", func(w http.ResponseWriter, r *http.Request) {
+		strPID := r.URL.Query().Get("pid")
+		strAddr := r.URL.Query().Get("addr")
+		if strPID == "" || strAddr == "" {
+			http.Error(w, "Missing pid or addr parameter", http.StatusBadRequest)
+			return
+		}
+		strPID = util.TrimHex(strPID)
+		strAddr = util.TrimHex(strAddr)
+
+		bPID, err := hex.DecodeString(strPID)
+		if err != nil {
+			http.Error(w, "Invalid pid parameter", http.StatusBadRequest)
+			return
+		}
+		pid := new(types.ProcessID).SetBytes(bPID)
+		if !pid.IsValid() {
+			http.Error(w, "Invalid process ID", http.StatusBadRequest)
+			return
+		}
+
+		addr := common.HexToAddress(strAddr)
+		proof, err := dummyCSP.GenerateProof(pid, addr)
+		if err != nil {
+			http.Error(w, "Failed to generate proof: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		res, err := json.Marshal(proof)
+		if err != nil {
+			http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	})
+
+	// Start the HTTP server
+	log.Println("Starting server on :" + strHTTPPort)
+	if err := http.ListenAndServe(":"+strHTTPPort, nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
